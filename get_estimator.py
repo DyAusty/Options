15a5c9aee5fa2079193c6f5b430c2e0d28164a43
Reality <94774818+DyAusty@users.noreply.github.com>
2024-10-08 12:36:31 +0300

    Create get_estimator.py

---
+++ get_estimator.py
@@ -0,0 +1,18 @@
+mport numpy as np
+
+def get_estimator(price_data, window=2, trading_periods=7, clean=True):
+    log_ho = (price_data['High'] / price_data['Open']).apply(np.log)
+    log_lo = (price_data['Low'] / price_data['Open']).apply(np.log)
+    log_co = (price_data['Close'] / price_data['Open']).apply(np.log)
+
+    log_oc = (price_data['Open'] / price_data['Close'].shift(1)).apply(np.log)
+    log_oc_sq = log_oc ** 2
+
+    log_cc = (price_data['Close'] / price_data['Close'].shift(1)).apply(np.log)
+    log_cc_sq = log_cc ** 2
+
+    rs = log_ho * (log_ho - log_co) + log_lo * (log_lo - log_co)
+
+    close_vol = log_cc_sq.rolling(window=window, center=False).sum() * (1.0 / (window - 1.0))
+    
+    return close_vol
